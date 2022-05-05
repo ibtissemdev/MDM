bb938cae71e13827d9fb9e947774e54b076fbb6c
ibtissemdev <ibtissem.khiri@gmail.com>
2022-05-05 16:54:53 +0200

    excel

--- api/test.bibli.php
+++ api/test.bibli.php
@@ -5,9 +5,18 @@ require '../vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+$fichier=scandir('input/');
+print_r ($fichier[2]);
+$fichier_a_traiter="input/".$fichier[2];
+print_r($fichier_a_traiter);
+
 $spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
+$classeur=\PhpOffice\PhpSpreadsheet\IOFactory::load($fichier_a_traiter);
+$sheet=$spreadsheet->getActiveSheet();
+
 $sheet->setCellValue('A1', 'Hello World !');
 
+
+
 $writer = new Xlsx($spreadsheet);
-$writer->save('hello world.xlsx');
+$writer->save($fichier[2].date('y_m_d').'.xlsx');
